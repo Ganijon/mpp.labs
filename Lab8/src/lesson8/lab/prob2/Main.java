61425189783a5c712437daf049bb884ccf5b0450
985565 <985565@CS_Lab115-208.cs.mum.edu>
2017-02-02 12:52:57 -0600

    Added lesson8.lab.prob2

---
+++ Lab8/src/lesson8/lab/prob2/Main.java
@@ -0,0 +1,30 @@
+package lesson8.lab.prob2;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class Main {
+    
+    static Consumer<String> printer = System.out::println;
+    
+    public static void main(String[] arg) {
+        printer.accept("Random numbers by:");
+
+        Supplier<Double> random1 = Math::random;
+        printer.accept(" i. Method Reference: " + random1.get());
+        
+        Supplier<Double> random2 = ()-> Math.random();
+        printer.accept(" ii. Lambda Expression: " + random2.get());
+        
+        Supplier<Double> random3 = new Main().new RandomSupplier();
+        printer.accept(" iii. Inner class: " + random3.get());
+    }
+    
+    //inner class implementation of Supplier<T>
+    private class RandomSupplier implements Supplier<Double> {
+        @Override
+        public Double get() {
+            return Math.random();
+        }
+    }
+}
