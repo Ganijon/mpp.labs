828362d265ec3d98d785f86cc56b4c23042b3750
985565 <985565@CS_Lab115-208.cs.mum.edu>
2017-02-03 16:02:16 -0600

    Added lesson9.lab.problem4

---
+++ Lab9/src/lesson9/lab/prob4/Main.java
@@ -0,0 +1,24 @@
+package lesson9.lab.prob4;
+
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSquares(4);
+        printSquares(7);
+        printSquares(10);
+    }
+
+    public static void printSquares(int num) {
+
+        IntUnaryOperator f = (n) -> n + 1;
+
+        IntStream
+                .iterate(1, f)
+                .limit(num)
+                .map(n -> n * n)
+                .forEach((n) -> System.out.printf("%s, ", n));
+    }
+}
